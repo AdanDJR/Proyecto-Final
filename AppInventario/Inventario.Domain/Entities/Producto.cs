5ff7c25d4ee2d699a0c7413f2eee99dd7f291912
Victor Fernandez <adanjesus679@gmail.com>
2025-05-29 01:01:44 -0400

    He terminado
    luego agrego lo que me farta

--- AppInventario/Inventario.Domain/Entities/Producto.cs
+++ AppInventario/Inventario.Domain/Entities/Producto.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Inventario.Domain.Entities
 {
-    class Producto
+    [Table("Producto")]
+    public class Productos
     {
+        
+        public int Id { get; set; }
+        public decimal Precio { get; set; }
+        public string Nombre { get; set; } = string.Empty;
     }
 }
---
+++ AppInventario/Inventario.Persistence/Context/InventarioDb.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inventario.Domain.Entities;
+
+namespace Inventario.Persistence.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Productos> Productos { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=VICTORFDJ;Database=DB_Inventario;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Productos>(entity =>
+            {
+                entity.Property(e => e.Precio)
+                      .HasColumnType("decimal(10,2)"); 
+            });
+        }
+    }
+}
